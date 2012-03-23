92c85263c6fec5e4325c7a8b3a1195016dbe8fdd
Jorge Chamorro Bieling <jorge@jorgechamorro.com>
2012-03-23 20:00:35 +0100

    ++puts() tests

--- test/test28_puts_nextTick_loop.js
+++ test/test28_puts_nextTick_loop.js
@@ -4,10 +4,11 @@ console.log("Using "+ i+ " threads.");
 
 function program () {
   var i= 0;
-  (function ƒ () {
+  function ƒ () {
     puts(" ["+ thread.id+ "]"+ (++i));
     thread.nextTick(ƒ);
-  })();
+  }
+  ƒ();
 }
 
 pool.all.eval(program).all.eval('program()');
--- test/test29_puts_callback_loop.js
+++ test/test29_puts_callback_loop.js
@@ -2,13 +2,13 @@ var i= parseInt(process.argv[2], 10) || 2;
 var pool= require('threads_a_gogo').createPool(i);
 console.log("Using "+ i+ " threads.");
 
-pool.on('again', function onAgain () {
-  this.eval('ƒ()');
-});
-
 function ƒ () {
   puts(" ["+ thread.id+ "]"+ (++i));
-  thread.emit('again');
+  //if (!(i%1e3)) gc();
 }
 
-pool.all.eval(ƒ).all.eval('i=0').all.eval('ƒ()');
+function callback (err, data) {
+  this.eval('ƒ()', callback);
+}
+
+pool.all.eval('i=0').all.eval(ƒ).all.eval('ƒ()', callback);

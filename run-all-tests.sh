b4d833fa55e677af7b679fb3442e2df996b255ec
Michael Nahkies <michael@nahkies.co.nz>
2018-02-07 23:00:57 +1300

    improve script to allow sigterm, timeout suites after 5s, and node-gyp rebuild

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-set -e
-
 OUTPUT="./run-all-tests-output.log"
 
 node ./test-package.js > ${OUTPUT}
 
-for filename in ./test/*.js; do
-    echo ${filename}
-    node "${filename}" >> ${OUTPUT}
-done
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
 
+trap _term SIGTERM
+
+for filename in ./test/*.js; do
+    timeout 5s node "${filename}" >> ${OUTPUT} &
+
+    child=$!
+    wait "${child}"
+
+    if [ $? = 0 ]; then
+        echo ${filename} "passed!"
+    else
+        echo ${filename} "failed!"
+    fi
+done

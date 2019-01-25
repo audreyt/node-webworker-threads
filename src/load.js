ad85faae398a99b4a1103b19812890425cdc069f
russa <aaron.russ@dfki.de>
2019-01-25 13:28:11 +0100

    importScripts: resolve relative path against WebWorker location instead of Current Working Directory
     * store location of WebWorker script as __filename
     * on importScripts: if imported path is no absolute URL, resolve against path of WebWorker's __filename
    
     * special treatment of non-URL paths (i.e. system file paths): "guess" (heuristic) if system file path is absolute
       * store Current Working Directory in WebWorker instance as __cwd
       * if system path is sub path to __cwd, treat as absolute (i.e. if __cwd is prefix of the path)
       * introduce escape character '!': if path starts with '!', treat the path as absolute path (i.e. do not resolve against WebWorker script location)

--- src/load.js
+++ src/load.js
@@ -7,9 +7,29 @@ function close(){
   return this.thread.emit('close');
 }
 function importScripts(){
-  var i$, len$, p, results$ = [];
+  var pos, i$, len$, p, escAbsolute, results$ = [];
+  pos = -1;
+  if (this.thread.__filename) {
+    pos = this.thread.__filename.lastIndexOf('/');
+    if (pos === -1) {
+      pos = this.thread.__filename.lastIndexOf('\\');
+    }
+  }
   for (i$ = 0, len$ = (arguments).length; i$ < len$; ++i$) {
     p = (arguments)[i$];
+    if (pos >= 0 && !/^(?:[a-z]+:)?\/\//i.test(p)) {
+      self.console.log("  resolving " + p + " against " + this.thread.__filename.substring(0, pos + 1) + ", cwd " + this.thread.__cwd);
+      if (p[0] === '!') {
+        escAbsolute = p[1] !== '!';
+        p = p.substring(1);
+        self.console.log("  ... removed escape character for absolute path: " + p);
+      }
+      if (!escAbsolute && (!this.thread.__cwd || p.indexOf(this.thread.__cwd) !== 0)) {
+        p = this.thread.__filename.substring(0, pos + 1) + p;
+      } else {
+        self.console.log("  ... do not resolve absolute " + p);
+      }
+    }
     results$.push(self.eval(native_fs_.readFileSync(p, 'utf8')));
   }
   return results$;
--- src/load.ls
+++ src/load.ls
@@ -3,7 +3,22 @@ function add-event-listener (event, cb)
 function close ()
     @thread.emit \close
 function importScripts ()
+    pos = -1
+    if @thread.__filename
+        pos = @thread.__filename.lastIndexOf '/'
+        if pos == -1
+            pos = @thread.__filename.lastIndexOf '\\'
     for p in arguments
+        if pos >= 0 && !/^(?:[a-z]+:)?\/\//i.test p
+            self.console.log("  resolving "+p+" against "+@thread.__filename.substring(0, pos + 1)+", cwd "+@thread.__cwd) #//DEBUG
+            if p[0] == '!'
+                escAbsolute = p[1] != '!'
+                p = p.substring 1
+                self.console.log("  ... removed escape character for absolute path: "+p) #//DEBUG
+            if !escAbsolute && (!@thread.__cwd || p.indexOf(@thread.__cwd) != 0)
+                p = @thread.__filename.substring(0, pos + 1) + p
+            else #//DEBUG
+                self.console.log("  ... do not resolve absolute "+p) #//DEBUG
         self.eval native_fs_.readFileSync(p, \utf8)
 onmessage = null
 thread.on \message (args) ~> onmessage? args
